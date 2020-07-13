c8560e386f5115106586b0da257f0d8e96e568af
Hyejin Lee <2hjinyj@gmail.com>
2020-07-13 19:53:44 +0900

    android

--- Android_Capstone/app/src/main/java/com/example/waistand/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.waistand;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Data.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract  DataDao dataDao();
-}
